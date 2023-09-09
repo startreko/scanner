8d4bd4aef2044120d616028d3c5d1e85c4820bde
startreko <100490385+startreko@users.noreply.github.com>
2023-09-09 22:32:53 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,63 +1,64 @@
 import sqlite3
 from datetime import datetime, timedelta
+import os
 
 class Database:
-    def __init__(self, file_path, scanned_code, machine_name):
+    def __init__(self, file_path, scanned_code, machine):
         self.file_path = file_path
         self.scanned_code = scanned_code
-        self.machine_name = machine_name
+        self.machine = machine
+
 
         conn = sqlite3.connect(self.file_path)
         cursor = conn.cursor()
 
-        try:
-            cursor.execute("""
-                CREATE TABLE IF NOT EXISTS Scans(
-                    code TEXT,
-                    machine_name TEXT,
-                    date_time TEXT
-                )
-            """)
-        finally:
-            conn.commit()
-            conn.close()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS Scans(
+                code TEXT,
+                machine TEXT,
+                date_time TEXT
+            )
+        """)
+
 
     def create_or_update_database(self):
+        
         conn = sqlite3.connect(self.file_path)
         cursor = conn.cursor()
 
-        try:
-            date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            cursor.execute("""
-                INSERT INTO Scans(code, machine_name, date_time) VALUES(?,?,?)
-            """, (self.scanned_code, self.machine_name, date_time))
-        finally:
-            conn.commit()
-            conn.close()
+        date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        cursor.execute("""
+            INSERT INTO Scans(code, machine, date_time) VALUES(?,?,?)
+        """, (self.scanned_code, self.machine, date_time))
+
+        conn.commit()
+        conn.close()
 
     def check_if_scanned(self, code):
+        
         conn = sqlite3.connect(self.file_path)
         cursor = conn.cursor()
 
-        try:
-            cursor.execute("""
-                SELECT date_time, machine_name FROM Scans WHERE code = ?
-            """, (code,))
-            result = cursor.fetchone()
-        finally:
-            conn.close()
+        cursor.execute("""
+            SELECT date_time, machine FROM Scans WHERE code = ?
+        """, (code,))
+
+        result = cursor.fetchone()
+        conn.close()
 
         return result
 
     def remove_old_entries(self, days_old):
+        
         conn = sqlite3.connect(self.file_path)
         cursor = conn.cursor()
 
-        try:
-            cutoff_date = (datetime.now() - timedelta(days=days_old)).strftime("%Y-%m-%d %H:%M:%S")
-            cursor.execute("""
-                DELETE FROM Scans WHERE date_time < ?
-            """, (cutoff_date,))
-        finally:
-            conn.commit()
-            conn.close()
+        cutoff_date = (datetime.now() - timedelta(days=days_old)).strftime("%Y-%m-%d %H:%M:%S")
+
+        cursor.execute("""
+            DELETE FROM Scans WHERE date_time < ?
+        """, (cutoff_date,))
+
+        conn.commit()
+        conn.close()
